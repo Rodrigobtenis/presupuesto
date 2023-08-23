02e2bc96e42e5cb049f0ddca7016074b0a94db91
Rodrigo Buscemi <rodrigobtenis@gmail.com>
2023-08-23 15:23:03 -0300

    mobile

--- js/presupuesto.js
+++ js/presupuesto.js
@@ -1,4 +1,4 @@
-let presupuestos = [];
+/* let presupuestos = [];
 let presupuestoActual = {
   cliente: "",
   metodoPago: "",
@@ -122,4 +122,145 @@ function nuevoPresupuesto() {
 function formatoMiles(numero) {
   return numero.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
+actualizarPresupuesto();
+ */
+
+
+
+let presupuestos = [];
+let presupuestoActual = {
+  cliente: "",
+  metodoPago: "",
+  productos: [],
+  totalEfectivo: 0,
+  totalTransferencia: 0,
+  totalPresupuesto: 0,
+};
+
+function cargarPresupuestos() {
+  const storedPresupuestos = localStorage.getItem("presupuestos");
+  if (storedPresupuestos) {
+    presupuestos = JSON.parse(storedPresupuestos);
+  }
+}
+
+function guardarPresupuestos() {
+  localStorage.setItem("presupuestos", JSON.stringify(presupuestos));
+}
+
+function agregarProducto() {
+  const cliente = document.getElementById("cliente").value.trim();
+  const producto = document.getElementById("producto").value.trim();
+  const precio = parseFloat(document.getElementById("precio").value);
+  const cantidad = parseInt(document.getElementById("cantidad").value);
+  const metodoPago = document.getElementById("metodoPago").value;
+
+  if (cliente === "" || producto === "" || isNaN(precio) || isNaN(cantidad)) {
+    alert("Por favor complete todos los campos correctamente.");
+    return;
+  }
+
+  let recargo = 0;
+  if (metodoPago === "transferencia") {
+    recargo = 0.1;
+  } else if (metodoPago === "tarjeta" || metodoPago === "cuotas") {
+    recargo = 0.25;
+  }
+
+  const totalUnitario = precio * cantidad;
+  const totalConRecargo = totalUnitario + totalUnitario * recargo;
+
+  const productoAgregado = {
+    cliente,
+    producto,
+    precio,
+    cantidad,
+    metodoPago,
+    totalUnitario,
+    totalConRecargo,
+  };
+
+  presupuestoActual.productos.push(productoAgregado);
+  actualizarPresupuesto();
+  guardarPresupuestos();
+  limpiarCampos();
+}
+
+function actualizarPresupuesto() {
+  const listaProductos = document.getElementById("listaProductos");
+  listaProductos.innerHTML = "";
+
+  let totalEfectivo = 0;
+  let totalTransferencia = 0;
+  let totalPresupuesto = 0;
+
+  presupuestoActual.productos.forEach((producto, index) => {
+    totalPresupuesto += producto.totalConRecargo;
+
+    listaProductos.innerHTML += `
+      <tr>
+        <td class="cliente-celda">${producto.cliente}</td>
+        <td>${producto.producto}</td>
+        <td>$${formatoMiles(producto.precio.toFixed(2))}</td>
+        <td>${producto.cantidad}</td>
+        <td>$${formatoMiles(producto.totalConRecargo.toFixed(2))}</td>
+        <td>
+          <button class="editar" onclick="editarProducto(${index})">Editar</button>
+          <button class="eliminar" onclick="eliminarProducto(${index})">Eliminar</button>
+        </td>
+      </tr>
+    `;
+
+    if (producto.metodoPago === "efectivo") {
+      totalEfectivo += producto.totalConRecargo;
+    } else {
+      totalTransferencia += producto.totalConRecargo;
+    }
+  });
+
+  presupuestoActual.totalEfectivo = totalEfectivo;
+  presupuestoActual.totalTransferencia = totalTransferencia;
+  presupuestoActual.totalPresupuesto = totalPresupuesto;
+
+  const totalEfectivoElement = document.getElementById("totalEfectivo");
+  totalEfectivoElement.textContent = `$${formatoMiles(totalEfectivo.toFixed(2))}`;
+
+  const totalTransferenciaElement = document.getElementById("totalTransferencia");
+  totalTransferenciaElement.textContent = `$${formatoMiles(totalTransferencia.toFixed(2))}`;
+}
+
+function limpiarCampos() {
+  document.getElementById("producto").value = "";
+  document.getElementById("precio").value = "";
+  document.getElementById("cantidad").value = "";
+  document.getElementById("metodoPago").value = "efectivo";
+}
+
+function editarProducto(index) {
+  const productoEditado = presupuestoActual.productos[index];
+  document.getElementById("cliente").value = productoEditado.cliente;
+  document.getElementById("producto").value = productoEditado.producto;
+  document.getElementById("precio").value = productoEditado.precio;
+  document.getElementById("cantidad").value = productoEditado.cantidad;
+  document.getElementById("metodoPago").value = productoEditado.metodoPago;
+
+  presupuestoActual.productos.splice(index, 1);
+  actualizarPresupuesto();
+}
+
+function eliminarProducto(index) {
+  presupuestoActual.productos.splice(index, 1);
+  actualizarPresupuesto();
+}
+
+function nuevoPresupuesto() {
+  // Recargar la página para reiniciar todo
+  location.reload();
+}
+
+function formatoMiles(numero) {
+  return numero.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
+cargarPresupuestos();
 actualizarPresupuesto();
